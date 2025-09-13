842cabde027a2fd8194b519b03faff8d8051c81e
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-09-13 20:12:44 -0300

    feat: Cria o mock de enpoints POST, GET, DELETE de Streaming

--- src/main/java/br/com/mmaverse/controller/StreamingController.java
+++ src/main/java/br/com/mmaverse/controller/StreamingController.java
@@ -1,4 +1,44 @@
 package br.com.mmaverse.controller;
 
+import br.com.mmaverse.entity.Streaming;
+import br.com.mmaverse.service.StreamingService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/mmaverse/streaming")
 public class StreamingController {
+
+    private final StreamingService streamingService;
+
+    public StreamingController(StreamingService streamingService) {
+        this.streamingService = streamingService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Streaming>> findAll() {
+        return ResponseEntity.ok(streamingService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<Streaming> save(@RequestBody Streaming streaming) {
+        Streaming savedStreaming = streamingService.save(streaming);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedStreaming);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Streaming> findById(@PathVariable Long id) {
+        return streamingService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        streamingService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
 }
