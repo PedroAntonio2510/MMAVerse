a13cb1f8ece493abbdb2bdff90af52dfc9509efd
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-09-13 09:30:39 -0300

    feat: Adicionando projeto

---
+++ src/main/java/br/com/mmaverse/MmaverseApplication.java
@@ -0,0 +1,13 @@
+package br.com.mmaverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmaverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmaverseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mmaverse/MmaverseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mmaverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmaverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
