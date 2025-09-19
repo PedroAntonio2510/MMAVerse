69232d4e452c4221b5fdf20a2f90f82fdcc62fa7
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-09-19 20:29:22 -0300

    fix: Atualiza o campo startDate do EventDTO para passar a data da forma dd/MM/yyyy-H:mm

--- src/main/java/br/com/mmaverse/dto/EventDTO.java
+++ src/main/java/br/com/mmaverse/dto/EventDTO.java
@@ -1,5 +1,6 @@
 package br.com.mmaverse.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import java.util.List;
 public record EventDTO(
         String name,
         String location,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy-H:mm")
         LocalDateTime startDate,
         List<Long> streamings,
         Long organization) {
