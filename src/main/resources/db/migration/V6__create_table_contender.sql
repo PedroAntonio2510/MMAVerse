4794e44b9a5e94c43252430d5f3cee8d9ccf3f8e
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-09-20 17:53:25 -0300

    feat: Cria migration de criação da tabela Contender

---
+++ src/main/resources/db/migration/V6__create_table_contender.sql
@@ -0,0 +1,16 @@
+CREATE TABLE contender(
+    id serial primary key,
+    name varchar(100) not null,
+    nickname varchar(50) not null,
+    cpf varchar(11) not null,
+    date_of_birth date not null,
+    age smallint,
+    weight_class varchar(50) not null,
+    height smallint not null,
+    reach smallint not null,
+    win smallint default 0,
+    lose smallint default 0,
+    draw smallint default 0,
+    created_at timestamp,
+    updated_at timestamp
+);
