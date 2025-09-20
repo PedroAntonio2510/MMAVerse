16efa35809efe5f02cd10a85edd05883c02d6140
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-09-20 18:00:11 -0300

    feat: Cria entidade Contender

---
+++ src/main/java/br/com/mmaverse/entity/Contender.java
@@ -0,0 +1,67 @@
+package br.com.mmaverse.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "contender")
+public class Contender {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "nickname", length = 50, nullable = false)
+    private String nickname;
+
+    @Column(length = 11, nullable = false)
+    private String cpf;
+
+    @Column(name = "date_of_birth", nullable = false)
+    private LocalDate birthDate;
+
+    private Integer age;
+
+    @Column(name = "weight_class", nullable = false, length = 50)
+    private String weightClass;
+
+    @Column(nullable = false)
+    private Integer height;
+
+    @Column(nullable = false)
+    private Integer reach;
+
+    private Integer win;
+
+    private Integer lose;
+
+    private Integer draw;
+
+    @ManyToMany
+    @JoinTable(name = "contender_gym",
+            joinColumns = @JoinColumn(name = "contender_id"),
+            inverseJoinColumns = @JoinColumn(name = "gym_id")
+    )
+    private List<Gym> gyms;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
