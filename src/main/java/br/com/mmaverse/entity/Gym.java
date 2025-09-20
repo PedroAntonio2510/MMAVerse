586b9d6c5c51d74249818f63fe2425eca36e6ce4
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-09-20 17:59:28 -0300

    feat: Cria e entidade Gym

---
+++ src/main/java/br/com/mmaverse/entity/Gym.java
@@ -0,0 +1,45 @@
+package br.com.mmaverse.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "gym")
+public class Gym {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Column(name = "location", length = 100, nullable = false)
+    private String location;
+
+    @Column(name = "foundation", nullable = false)
+    private Integer foundation;
+
+    @ManyToMany(mappedBy = "gyms")
+    private List<Contender> contenders;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+}
