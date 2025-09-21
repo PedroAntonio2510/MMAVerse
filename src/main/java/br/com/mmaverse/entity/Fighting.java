d92547e76e316ec94b7be05b1f253859f1b4501c
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-09-21 11:33:52 -0300

    feat: Cria a entity Fighting

---
+++ src/main/java/br/com/mmaverse/entity/Fighting.java
@@ -0,0 +1,55 @@
+package br.com.mmaverse.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "fighting")
+public class Fighting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "contender_red_corner_id", nullable = false)
+    private Contender contenderRedCorner;
+
+    @ManyToOne
+    @JoinColumn(name = "contender_blue_corner_id", nullable = false)
+    private Contender contenderBlueCorner;
+
+    @ManyToOne
+    @JoinColumn(name = "contender_winner_id")
+    private Contender winner;
+
+    @Column(name = "method_of_victory", length = 50)
+    private String methodOfVictory;
+
+    @Column(name = "end_round")
+    private Integer endRound;
+
+    @Column(name = "end_time")
+    private String endTime;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+}
