e39024b4ac3690a367de6c10cf7bea13b2a4ac67
PedroAntonio2510 <pedroantoniofla10@gmail.com>
2025-10-19 14:48:13 -0300

    feat: Cria a migrantion da entidade User e faz a migration do banco de dados

---
+++ src/main/java/br/com/mmaverse/entity/User.java
@@ -0,0 +1,24 @@
+package br.com.mmaverse.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
